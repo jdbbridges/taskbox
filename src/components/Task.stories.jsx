bd8296cdabbdf9876cdb9c4a6b8274c0737a418f
jdbbridges <jdb.bridges@gmail.com>
2024-03-10 15:05:06 -0400

    extra long string attempt 2

--- src/components/Task.stories.jsx
+++ src/components/Task.stories.jsx
@@ -1,5 +1,7 @@
 import Task from './Task';
 
+const longTitleString = `This task's name is absurdly large. In fact, I think if I keep going I might end up with content overflow. What will happen? The star that represents a pinned task could have text overlapping. The text could cut-off abruptly when it reaches the star. I hope not!`;
+
 export default {
   component: Task,
   title: 'Task',
@@ -32,8 +34,6 @@ export const Archived = {
       state: 'TASK_ARCHIVED',
     },
   },
-
-const longTitleString = `This task's name is absurdly large. In fact, I think if I keep going I might end up with content overflow. What will happen? The star that represents a pinned task could have text overlapping. The text could cut-off abruptly when it reaches the star. I hope not!`;
 
 export const LongTitle = {
   args: {
